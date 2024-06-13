985d364fc358d06a0a74229c1b6dddce7fd15b0a
charlieop <spchanae@connect.ust.hk>
2024-06-13 18:01:12 +0800

    importing settings for amplify

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json"
+
+Amplify.configure(outputs);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +19,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
