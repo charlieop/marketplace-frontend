832c29a0ad1b9a0f57273ad9a3c22d15add616ba
charlieop <spchanae@connect.ust.hk>
2024-06-13 18:40:28 +0800

    migrated to vite

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json"
